429c00c66ac90d728024e452bb7c89fdba9f657c
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-05-11 09:55:52 +0700

    add

---
+++ We17303/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular';
+  myName: string = 'Derek';
+  myAge: number = 30;
+  myStatus: boolean = true;
+  myInfo: { name: string, age: number } = {
+    name: "Derek",
+    age: 30
+  };
+  showInfo() {
+    return this.myAge + 10;
+  }
+}
+
